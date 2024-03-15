8e8bf6533a397f9a1bd64865db5754e42bf5b197
Urban Hernandez <92275709+Urban-Hacker@users.noreply.github.com>
2024-03-16 00:32:29 +0100

    update the scene to run at startup

--- Scenes/screens/welcome_screen.gd
+++ Scenes/screens/welcome_screen.gd
@@ -6,7 +6,7 @@ var _version_patch:int
 func _ready():
 	_version_main = ProjectSettings.get("application/config/version_main")
 	_version_patch =  ProjectSettings.get("application/config/version_patch")
-	$GUI/VersionButton.text = "Ver: " + str(_version_main) + "." + str(_version_patch)
+	$GUI/VersionButton.text = "> Ver: " + str(_version_main) + "." + str(_version_patch)
 
 
 func _process(delta):
