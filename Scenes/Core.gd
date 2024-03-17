d40b0bf2454b78f90810598de18eab3687a8d228
Urban Hernandez <92275709+Urban-Hacker@users.noreply.github.com>
2024-03-18 00:02:17 +0100

    move md parser to core

--- Scenes/Core.gd
+++ Scenes/Core.gd
@@ -39,3 +39,29 @@ func _pad(string_to_pad:String, length:int) -> String:
 	for i in range(0, length - len(string_to_pad)):
 		padding += "0"
 	return padding + string_to_pad
+
+func markdown_to_bbcode(markdown: String) -> String:
+	var bbcode = ""
+	var lines = markdown.split("\n")
+	var in_list = false
+
+	for line in lines:
+		if line.strip_edges().begins_with("# "):
+			bbcode += "" + line.replace("\n", "") + "\n" + "=".repeat(len(line)) + "\n"
+			continue
+		if line.strip_edges().begins_with("* "):
+			if not in_list:
+				bbcode += "[ul]"
+				in_list = true
+			bbcode += line.replace("* ", "") + "\n"
+		else:
+			if in_list:
+				bbcode += "[/ul]\n"
+				bbcode = bbcode.replace("\n[/ul]", "[/ul]")
+				in_list = false
+			bbcode += line + "\n"
+
+	if in_list:
+		bbcode += "[/ul]\n"
+
+	return bbcode.strip_edges()
