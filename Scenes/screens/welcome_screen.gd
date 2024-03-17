f7319c33a68588173175ed28cfa026008f1a8a03
Urban Hernandez <92275709+Urban-Hacker@users.noreply.github.com>
2024-03-17 23:49:22 +0100

    add change log for version 0.2

--- Scenes/screens/welcome_screen.gd
+++ Scenes/screens/welcome_screen.gd
@@ -5,12 +5,14 @@ enum _STATES {welcome, changelog}
 var _version_main:int
 var _version_patch:int
 var _version_string:String
+var _changelog_file:String
 var _current_state:_STATES
 
 func _ready():
 	_version_main = ProjectSettings.get("application/config/version_main")
 	_version_patch =  ProjectSettings.get("application/config/version_patch")
 	_version_string = str(_version_main) + "." + str(_version_patch)
+	_changelog_file = str(_version_main) + "_" + str(_version_patch) + ".md"
 	$GUI/ChangelogButton.text = "> Ver: " + _version_string
 	SaveSystem.set_var("slot", 1)
 	_change_state(_STATES.welcome)
@@ -28,7 +30,7 @@ func _change_state(state:_STATES):
 		$GUI/Changelog/Terminal.clear_buffer()
 		$GUI/Changelog/Terminal.load_file("res://Changelogs/logo.txt")
 		$GUI/Changelog/Terminal.add_line("Current Version: " + _version_string + "\n\n")
-		$GUI/Changelog/Terminal.load_file("res://Changelogs/0_1.md", true)
+		$GUI/Changelog/Terminal.load_file("res://Changelogs/" + _changelog_file, true)
 		$GUI/Changelog.show()
 
 func _on_login_button_pressed():
