fb86a2bd0c4672550f25bd14c04498c73af19857
Urban Hernandez <92275709+Urban-Hacker@users.noreply.github.com>
2024-03-25 22:53:12 +0100

    fully working prototype with quest system

---
+++ Quests/probation_period_quests/DayOne.gd
@@ -0,0 +1,19 @@
+extends Quest
+class_name DDoSQuest
+
+@export var _smartContracts:int
+@export var _level:PackedScene
+
+func launch() -> void:
+	Core.get_tree().change_scene_to_packed(_level)
+
+func start() -> void:
+	print("Quest Started")
+
+func complete() -> void:
+	print("Quest Completed")
+
+func another_cool_function(quest_id):
+	print(quest_id)
+	if quest_id == self.id:
+		objective_completed = true
