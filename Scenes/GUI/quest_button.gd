1c1d71dd4916fa5a9a4bbadb14c4d64e5c2e724b
Urban Hernandez <92275709+Urban-Hacker@users.noreply.github.com>
2024-03-26 15:09:36 +0100

    add new shader for btn + improve quest selector

--- Scenes/GUI/quest_button.gd
+++ Scenes/GUI/quest_button.gd
@@ -4,7 +4,7 @@ var _quest:Quest
 
 func set_quest(quest) -> void:
 	_quest = quest
-	$Button.text = _quest.quest_name
+	$Button.text = "    " + _quest.quest_name
 	$Button.button_down.connect(_on_button_down) 
 
 func _on_button_down():
